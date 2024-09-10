e3746df76fb190551a60768eaab5e83e4b2fd14d
Steve <oseguera.steven@outlook.com>
2024-09-10 09:03:28 -0700

    - Add Material UI - Updated navigation with Material UI buttons.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-navigation',
   standalone: true,  // This makes the component standalone
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButton],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
