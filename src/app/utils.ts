b0949665f5a110c60ca2e66cf55c177c455cb4a1
Eric de Guzman <eric_deguzman@bmc.com>
2024-10-07 12:49:29 -0700

    Auto-generated commit

--- src/app/utils.ts
+++
@@ -1,5 +0,0 @@
-export const stripHtml = (html?: string): string => {
-  const div = document.createElement('div');
-  div.innerHTML = html? html : '';
-  return div.textContent || div.innerText || '';
-}
