87d7c31c5ce8e8f81c8d7515517965c2ea69fa02
Eric de Guzman <eric_deguzman@bmc.com>
2024-10-07 12:29:48 -0700

    aux-ng --> aux-ng-dev

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'aux-ng' title`, () => {
+  it(`should have the 'aux-ng-dev' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('aux-ng');
+    expect(app.title).toEqual('aux-ng-dev');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, aux-ng');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, aux-ng-dev');
   });
 });
