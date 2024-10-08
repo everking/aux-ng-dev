c3e7764c0b1d0003324b896a62bb652278f62141
Eric de Guzman <eric_deguzman@bmc.com>
2024-10-07 17:01:43 -0700

    GH Actions fetch articles.

---
+++ .github/workflows/scripts/fetch-firestore-articles.js
@@ -0,0 +1,28 @@
+(async () => {
+  const fs = require('fs');  
+  const queryUrl = 'https://firestore.googleapis.com/v1/projects/auxilium-420904/databases/aux-db/documents:runQuery';
+
+  try {
+    const response = await fetch(queryUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        structuredQuery: {
+          from: [{ collectionId: 'articles' }]
+        }
+      })
+    });
+    
+    if (!response.ok) {
+      throw new Error(`Error fetching Firestore data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    fs.writeFileSync('src/app/all-articles.json', JSON.stringify(data, null, 2));
+  } catch (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+})();
